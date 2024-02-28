657992bd881a06c8cfb481978a7356b8d57116ed
ImAidanJ <aidanjhamblin@gmail.com>
2024-02-27 23:55:10 -0500

    #1.2.2 Cant figure out chat:addSuggestion. Ive tried everything possible.

--- client/client.lua
+++ client/client.lua
@@ -2,12 +2,26 @@
 
 RegisterCommand("pos", function(source, args)
     TriggerServerEvent('SaveCoords', args[1])
-    TriggerEvent('chat:addSuggestion', '/pos', 'Save Current Coords to File.', {{ name='Name', help='Adds a Name in Coords File.'}})
 end)
 
 -- ClearCoords Command --
 
 RegisterCommand("clearcoords", function(source, args)
     TriggerServerEvent('ClearCoords')
-    TriggerEvent('chat:addSuggestion', '/clearcoords', 'Clears the Coords File.', {})
 end)
+
+--[[ Chat Suggestions
+
+This don't work and I have no Idea why. I've compared it to other scripts and forums post it just wont work I don't get it!
+
+Citizen.CreateThread(function()
+
+    TriggerEvent('chat:addSuggestion', '/pos', 'Save Current Coords to File.', {
+        { name='Name', help='Adds a Name in Coords File.'}
+    })
+
+    TriggerEvent('chat:addSuggestion', '/clearcoords', 'Clears the Coords File.')
+end)
+
+
+--]]
