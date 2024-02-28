2855178d18d9e29a5aae763758627c41758fd78b
ImAidanJ <aidanjhamblin@gmail.com>
2024-02-27 19:34:11 -0500

    Revert "#1.1.2 | Cleaned Up and Added Chat Suggestions"
    This reverts commit 8e054f917d658f8046f335629f3b49ba9212c594.

--- config.lua
+++ config.lua
@@ -1,11 +1,7 @@
 Config = {
-
+ChatPrefix = "^4[AJ:Coords]^0 ", -- Message Prefix
 UseAcePermissions = false, -- Set to true to turn them on
-
 AcePermissionString = "AJ.Coords", -- If you have questions DM me (iamaidanj) on discord. Also wont work unless AcePerms are on
-
-FileName = "coordinates.txt", -- The Name of the Coords File. I suggest leaving at the default name but it's up to you.
-
 }
 
 
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,9 +3,9 @@ fx_version 'cerulean'
 game 'gta5'
 author 'ImAidanJ'
 description 'Simple FiveM Script that saves coords to a file.'
-version '1.1.2'
+version '1.1.1' -- Most Likey Final
 
 server_script {
-    'config.lua',
-    'server/server.lua'
+    'config.lua', -- LEAVE AT THE TOP
+    'main.lua',
 }
